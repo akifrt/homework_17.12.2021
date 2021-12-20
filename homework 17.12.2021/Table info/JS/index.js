83c9ad32884b752d277b4108acd92677fbb872b5
akifrt <91149807+akifrt@users.noreply.github.com>
2021-12-20 17:31:02 +0400

    hm

---
+++ homework 17.12.2021/Table info/JS/index.js
@@ -0,0 +1,82 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["Name"] = document.getElementById("Name").value;
+    formData["Surname"] = document.getElementById("Surname").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["Photo"] = document.getElementById("Photo").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.Name;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.Surname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.salary;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.Photo;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+					   
+					   
+}
+
+function resetForm() {
+    document.getElementById("Name").value = "";
+    document.getElementById("Surname").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("Photo").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("Name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("Surname").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("Photo").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.Name;
+    selectedRow.cells[1].innerHTML = formData.Surname;
+    selectedRow.cells[2].innerHTML = formData.salary;
+    selectedRow.cells[3].innerHTML = formData.Photo;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("Name").value == ""){
+        isValid = false;
+		alert("Please input Name.");
+        
+    } else {
+        isValid = true;
+        if (!document.getElementById("NameValidationError").classList.contains("hide"))
+            document.getElementById("NameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
